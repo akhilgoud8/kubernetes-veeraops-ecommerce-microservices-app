a6420c3bbbeb20991948784f6d17807e1fa1d943
Akhil merugu <93387094+akhilgoud8@users.noreply.github.com>
2026-09-19 09:46:45 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devncloudtechdevops"
+    bucket       = "asuskubectlakmevivo"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
